ede104bd6bcf0c16cb7cfdc31faafbe8a2952e15
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-08-29 19:15:10 -0400

    import done

--- IndexedSeq.java
+++ IndexedSeq.java
@@ -1,3 +1,12 @@
+/*
+    This class is an IndexedSeq object.
+    - The purpose of the object is to keep the original index of the sequence during sorting.
+    - Array with identifiers and sequences can be arranged directly with the order of the original indices
+    after sorting
+
+    parameters: original index before sorting, sequence at the index
+
+*/
 public class IndexedSeq{
     private int index;
     private String sequence;
--- sortFastq.java
+++ sortFastq.java
@@ -4,53 +4,56 @@ import java.util.Scanner;
 
 public class sortFastq{
 
-    private static final int MAX = 2000000;
-    private static final int SUBLENGTH = 4;
+    private static final int MAX = 2000000; // maximum read 2 million
+    private static final int SUBLENGTH = 4; // 4 sub-string per read
     private static String [][] identifiers = new String [MAX][SUBLENGTH];//2D array, single set of read store in one subarray
     private static IndexedSeq [] sequences = new IndexedSeq[MAX];//sequence only, with index
     
+    //main class
     public static void main(String[] args) throws FileNotFoundException {
 
         IndexedSeq [] unsortedSeq = importFile("/Users/rx32940/Documents/sample1k.fastq"); 
         System.out.println(identifiers[100][2]);   
     }
 
+    //function for file import
     public static IndexedSeq [] importFile(String newFile) throws FileNotFoundException {
         
-        File file = new File(newFile);
-        Scanner reader = new Scanner(file);
+        File file = new File(newFile); // new file
+        Scanner reader = new Scanner(file); //reader
         
-        IndexedSeq [] unsorted = new IndexedSeq [MAX];
+        IndexedSeq [] unsorted = new IndexedSeq [MAX]; // array with IndexedSeq object
 
-        int i =0;
-        int j =0;
+        int i =0;//index of the reads
+        int j =0;//index of the line in each read
         
-        String current = "";
+        String current = ""; // current line reading
         while(reader.hasNextLine()){
             
-            if(j <4){
-                current = reader.nextLine();
-                identifiers[i][j] = current; 
-                if(j==1){
-                    unsorted[i] = new IndexedSeq(i, current);
+            if(j <4){// still in current read
+                current = reader.nextLine(); //read the line
+                identifiers[i][j] = current; //save the current line in full table
+                if(j==1){// line with seq
+                    unsorted[i] = new IndexedSeq(i, current);// save the seq and index to the seq array
                 }
-            j++;
+            j++;//to next line inside the read
             }  
             else{
-                j=0;
-                i++;
+                j=0; //to new read, from first line 
+                i++; //to new read
             }
         
             
         }
 
 
-        reader.close();
+        reader.close();//close the reader
 
         return unsorted;
 
     }
-
+    
+    //sort the sequence
     public static void quickSort(){
 
 
